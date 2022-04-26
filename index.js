fa5d08b1a795a7b40ada452a3e0a16503b1b69cb
Rory <rorygran5@gmail.com>
2022-04-25 22:34:42 -0400

    commit tree 5aaa461bffcec80abb3c8e2a3a0859955dc0ee2b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(phrase = "roller-skate"){
+    return `This Saturday, I want to ${phrase}!`;
+}
+
+function mondayWork(phrase = "go to the office"){
+    return `This Monday, I will ${phrase}.`;
+}
+
+function wrapAdjective(flair = '*'){
+    const mess = function (msg = 'special'){
+        return `You are ${flair}${msg}${flair}!`;
+    }
+
+    return mess;
+}
